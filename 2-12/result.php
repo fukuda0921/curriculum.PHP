9e3de66b920318a723f35919e3484942821a6a70
fukuda0921 <mitsu0921.fukuda@i.softbank.jp>
2021-07-20 04:14:15 +0900

    2-12課題提出

---
+++ 2-12/result.php
@@ -0,0 +1,8 @@
+<?php
+    $my_name = $_POST['my_name'];
+    $radio = $_POST['rank'];
+    $number = $_POST['number'];
+?>
+    <p>お名前：<?php echo $my_name; ?></p>
+    <p>ご希望商品<?php echo $radio; ?></p>
+    <p>個数：<?php echo $number; ?></p>
